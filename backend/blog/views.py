6f7474a04501d4f550921624b90921ed885ba7a3
ehdgns565 <ehdgns322@gmail.com>
2022-09-17 19:11:22 +0900

    [블로그] 1. view에 동적데이터 전달 2. css 적용

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # posts 쿼리셋 객체를 post_list.html 템플릿에 전달
+    return render(request, 'blog/post_list.html', {'posts': posts})
