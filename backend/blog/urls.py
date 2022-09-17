640ba9a06c81a585b7e6b4a67e6b469028c14109
ehdgns565 <ehdgns322@gmail.com>
2022-09-17 23:14:34 +0900

    [블로그] 블로그 글 상세내용 페이지 진입 링크 연결하기

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # post_list라는 view를 루트 URL에 할당
     # 루트 웹사이트 주소로 들어왔을때 views.post_list를 보여주게 됨
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -7,3 +7,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # posts 쿼리셋 객체를 post_list.html 템플릿에 전달
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
