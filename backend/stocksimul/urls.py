27695fc743e7c6b028d6ed6ef5cc819303ebf989
ehdgns565 <ehdgns322@gmail.com>
2022-09-18 22:19:30 +0900

    [주식시뮬레이터] 1. stock_simul_param > stock_simul_result 로 redirect 시 입력폼을 DB에 저장하고, pk 넘겨주도록 수정. 2. stock_simul_result URL에 pk 인자 추가 (없으면 에러남)

--- backend/stocksimul/urls.py
+++ backend/stocksimul/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.stock_simul_param, name='stock_simul_param'),
-    path('stock/simul/result', views.stock_simul_result, name='stock_simul_result')
+    path('stock/simul/result/<int:pk>/', views.stock_simul_result, name='stock_simul_result')
 ]
--- backend/stocksimul/views.py
+++ backend/stocksimul/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from .forms import StockSimulParamForm
+from .models import StockSimulParam
 
 
 def stock_simul_param(request):
@@ -9,13 +10,13 @@ def stock_simul_param(request):
         form = StockSimulParamForm(request.POST)
         if form.is_valid():  # 모든 필드에 값이 있어야 하고, 잘못된 값이 있다면 저장되지 않도록 체크.
             stock = form.save(commit=False)  # 폼을 저장하지만, 바로 모델에 저장하지 않도록 commit옵션 False
-            event_name = stock.event_name
-            start_date = stock.start_date
-            return redirect('stock_simul_result')
+            stock.save()
+            return redirect('stock_simul_result',pk=stock.pk)
     else:
         form = StockSimulParamForm()
     return render(request, 'stocksimul/stock_simul_param.html', {'form': form})
 
 
-def stock_simul_result(request):
-    return render(request, 'stocksimul/stock_simul_result.html')
+def stock_simul_result(request, pk):
+    stock = get_object_or_404(StockSimulParam, pk=pk)
+    return render(request, 'stocksimul/stock_simul_result.html', {'stock':stock})
