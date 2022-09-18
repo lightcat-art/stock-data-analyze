c90a93fbb74394f6d7a3eb00c2c6388fb7875a8b
ehdgns565 <ehdgns322@gmail.com>
2022-09-18 13:20:25 +0900

    [주식시뮬레이터] stocksimul 앱 추가. url 추가 ( 기본 홈 접속 경로가 stocksimul이 되도록 수정)

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # post_list라는 view를 루트 URL에 할당
     # 루트 웹사이트 주소로 들어왔을때 views.post_list를 보여주게 됨
-    path('', views.post_list, name='post_list'),
+    path('post/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new',views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
