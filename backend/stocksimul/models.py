a8500e4199f3d6cdf146ba1a4b984f679b523fdb
ehdgns565 <ehdgns322@gmail.com>
2022-09-26 01:44:39 +0900

    [주식시뮬레이터] 1. querySet SELECT 데이터가 없을 시 None으로 체크하지 않고 count()를 이용하도록 버그 수정 2. 종목코드와 주가정보 데이터 INSERT 시에 한번에 commit하도록 transaction.atomic 이용 3. Model ForeighKey 컬럼명이 이상하게 설정되는 오류 수정. -> _id 가 기본으로 항상 뒤에 붙게 되므로 고려하여 설정

--- backend/stocksimul/models.py
+++ backend/stocksimul/models.py
@@ -28,8 +28,8 @@ class StockEvent(models.Model):
 
 
 class StockSimulResult(models.Model):
-    stock_simul_param_id = models.ForeignKey("StockSimulParam", on_delete=models.CASCADE)
-    stock_event_id = models.ForeignKey("StockEvent", on_delete=models.CASCADE)
+    stock_simul_param = models.ForeignKey("StockSimulParam", on_delete=models.CASCADE)
+    stock_event = models.ForeignKey("StockEvent", on_delete=models.CASCADE)
     max_rate = models.FloatField(default=0)
     max_days_taken = models.IntegerField(default=0)
     min_rate = models.FloatField(default=0)
@@ -39,7 +39,7 @@ class StockSimulResult(models.Model):
 class StockPrice(models.Model):
     # 기본적으로 django가 auto-increment id를 생성하기 때문에, 명시적으로 생성할 pk에는 primary_key 옵션을 넣어주어야 한다.
     stock_price_id = models.BigAutoField(primary_key=True)
-    stock_event_id = models.ForeignKey("StockEvent", on_delete=models.CASCADE)
+    stock_event = models.ForeignKey("StockEvent", on_delete=models.CASCADE)
     date = models.DateField()
     open = models.IntegerField(default=-1, null=True)
     close = models.IntegerField(default=-1, null=True)
@@ -58,7 +58,7 @@ class StockInfoUpdateStatus(models.Model):
     table_type = models.CharField(max_length=1)  # 'P' : 주가테이블, 'E' : 종목코드테이블
     detail_info_1 = models.BigIntegerField(null=True)  # 각 테이블 filtering에 필요한 big integer 정보 입력
     detail_info_2 = models.CharField(max_length=200, null=True)  # 각 테이블 filtering에 필요한 char 정보 입력
-    mod_dt = models.DateField(default=timezone.now())
+    mod_dt = models.DateField(default=timezone.now)
     reg_dt = models.DateField()
 
     objects = models.Manager()
--- backend/stocksimul/views.py
+++ backend/stocksimul/views.py
@@ -13,6 +13,7 @@ from .models import StockPrice, StockInfoUpdateStatus, StockEvent
 from pykrx.website.krx.market import wrap
 from pykrx import stock
 
+
 def stock_simul_param(request):
     if request.method == "POST":
         form = StockSimulParamForm(request.POST)
@@ -25,7 +26,6 @@ def stock_simul_param(request):
     return render(request, 'stocksimul/stock_simul_param.html', {'form': form})
 
 
-@transaction.atomic
 def stock_simul_result(request, pk):
     simul_param = get_object_or_404(StockSimulParam, pk=pk)
     print(simul_param.start_date)
@@ -43,54 +43,63 @@ def stock_simul_result(request, pk):
 
     event_info_status = StockInfoUpdateStatus.objects.filter(table_type='E')
     print('event_info_status={}'.format(event_info_status))
+    print('event_info_status count={}'.format(event_info_status.count()))
     print('current time={}'.format(datetime.datetime.now()))
-    print('last mod date = {}'.format(event_info_status.mod_dt))
-    print(datetime.datetime.now() - event_info_status.mod_dt)
+    if event_info_status.count() != 0:
+        print('last mod date = {}'.format(event_info_status.mod_dt))
+        print(datetime.datetime.now() - event_info_status.mod_dt)
 
-    if event_info_status is None or (datetime.datetime.now() - event_info_status.mod_dt) \
+    if event_info_status.count() == 0 or (datetime.datetime.now() - event_info_status.mod_dt) \
             > timedelta(days=EVENT_INFO_UPDATE_PERIOD):
         whole_code_df = wrap.get_market_ticker_and_name(date=start_date, market='ALL')
         whole_code_df = [{'event_code': k, 'event_name': v} for k, v in whole_code_df.iteritems()]
-        for item in whole_code_df:
-            entry = StockEvent(**item)
-            entry.save()
+        print('1')
+        with transaction.atomic():
+            for item in whole_code_df:
+                entry = StockEvent(**item)
+                entry.save()
+        print('2')
 
-        if event_info_status is None:
-            status_dict = {'table_type':'E', 'mod_dt':datetime.datetime.now(),'reg_dt':datetime.datetime.now()}
+        if event_info_status.count() == 0:
+            status_dict = {'table_type': 'E', 'mod_dt': datetime.datetime.now(), 'reg_dt': datetime.datetime.now()}
             event_info_status = StockInfoUpdateStatus(**status_dict)
         else:
             event_info_status.mod_dt = datetime.datetime.now()
         event_info_status.save()
+        print('3')
     else:
         print("event_info_status doesn't have to update. last update date is {}".format(event_info_status.mod_dt))
 
     event_info = get_object_or_404(StockEvent, event_name=event_name)
-
+    print('event_name={}, event_info={}'.format(event_name, event_info))
+    print('stock_event_id = {}'.format(event_info.stock_event_id))
     # 종목별 주가정보 업데이트 날짜주기
     PRICE_INFO_UPDATE_PERIOD = 1
     today = datetime.datetime.now().strftime('%Y%m%d')
     price_info_status = StockInfoUpdateStatus.objects.filter(table_type='P') \
         .filter(detail_info_1=event_info.stock_event_id)
-    update_start_dt = (price_info_status.mod_dt + timedelta(days=1)).strftime('%Y%m%d')
-    if price_info_status is None or (datetime.datetime.now() - price_info_status.mod_dt) \
+    if price_info_status.count() == 0 or (datetime.datetime.now() - price_info_status.mod_dt) \
             > timedelta(days=PRICE_INFO_UPDATE_PERIOD):
-        info_none_yn = price_info_status is None
+        info_none_yn = price_info_status.count() == 0
         if info_none_yn:
-            price_info_df = stock.get_market_ohlcv_by_date('2000-01-01', today, event_info.event_code)
+            update_start_dt = '2000-01-01'
         else:
-            price_info_df = stock.get_market_ohlcv_by_date(update_start_dt, today, event_info.event_code)
+            update_start_dt = (price_info_status.mod_dt + timedelta(days=1)).strftime('%Y%m%d')
+        price_info_df = stock.get_market_ohlcv_by_date(update_start_dt, today, event_info.event_code)
         price_info_df = price_info_df.reset_index()
-        price_info_df.rename(columns={'날짜': 'date', '시가': 'open', '종가': 'close', '거래량': 'volume', '고가': 'high', '저가': 'low'},
-                       inplace=True)
+        price_info_df.rename(
+            columns={'날짜': 'date', '시가': 'open', '종가': 'close', '거래량': 'volume', '고가': 'high', '저가': 'low'},
+            inplace=True)
+        with transaction.atomic():
+            for item in price_info_df.to_dict('records'):
+                # if StockPrice.objects.filter(date=item['date']).filter(event_code=event_info.event_code).count() < 1:
+                entry = StockPrice(**item)
+                entry.event_code = event_info.event_code
+                entry.stock_event_id = event_info.stock_event_id
+                entry.save()
 
-        for item in price_info_df.to_dict('records'):
-            # if StockPrice.objects.filter(date=item['date']).filter(event_code=event_info.event_code).count() < 1:
-            entry = StockPrice(**item)
-            entry.event_code = event_info.event_code
-            entry.save()
-
-    simul_start_date = start_date.strptime('%Y%m%d')
-    simul_end_date = end_date.strptime('%Y%m%d')
+    simul_start_date = datetime.datetime.strptime(start_date, '%Y%m%d')
+    simul_end_date = datetime.datetime.strptime(end_date, '%Y%m%d')
     stocks = StockPrice.objects.filter(event_code=event_info.event_code) \
         .filter(date__gte=simul_start_date, date_lte=simul_end_date).order_by('date')
 
