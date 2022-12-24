6e7c8b52153fe50e073e332bd44b6e6ef5d92522
ehdgns565 <ehdgns322@gmail.com>
2022-12-25 06:53:53 +0900

    1. 홈 버튼 문구 변경 2. blog app 관련 url 차단.

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     # post_list라는 view를 루트 URL에 할당
     # 루트 웹사이트 주소로 들어왔을때 views.post_list를 보여주게 됨
-    path('post/', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new',views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    # path('post/', views.post_list, name='post_list'),
+    # path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # path('post/new',views.post_new, name='post_new'),
+    # path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
