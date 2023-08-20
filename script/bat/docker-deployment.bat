57eace05aff278bc2505d185a58dd0322599d700
ehdgns565 <ehdgns322@gmail.com>
2023-08-21 02:43:15 +0900

    docker compose network 명 변경

--- script/bat/docker-deployment.bat
+++ script/bat/docker-deployment.bat
@@ -7,7 +7,7 @@ docker load -i stock-data-analyze-db.tar
 docker load -i stock-data-analyze-web.tar
 
 rem run db&app
-docker run -itd -p 4306:3306 --network stock-data-analyze-default --ip 172.21.0.30 --name stock-data-analyze-db stock-data-analyze-db:latest
-docker run -itd -p 8100:8100 --network stock-data-analyze-default --ip 172.21.0.40 --name stock-data-analyze-web stock-data-analyze-web:latest
+docker run -itd -p 4306:3306 --network stock-data-analyze_network-default --ip 172.21.0.30 --name stock-data-analyze-db stock-data-analyze-db:latest
+docker run -itd -p 8100:8100 --network stock-data-analyze_network-default --ip 172.21.0.40 --name stock-data-analyze-web stock-data-analyze-web:latest
 
 
