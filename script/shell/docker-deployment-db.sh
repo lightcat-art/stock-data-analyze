6f72f477640b3d7cfd4240f147e8405ec97e755b
ehdgns565 <ehdgns322@gmail.com>
2023-09-16 22:13:45 +0900

    $HOME은 보통 linux에 이미 설정되어있음을 감안하여 script에서 생략

--- script/shell/docker-deployment-db.sh
+++ script/shell/docker-deployment-db.sh
@@ -1,4 +1,5 @@
-HOME=/home/ec2-user
+#HOME은 보통 linux에 default로 설정되어 있음.
+#HOME=/home/ec2-user
 sudo docker network create --ipam-driver default --subnet 172.21.0.10/16 --gateway 172.21.0.20 stock-data-analyze_network-default
 
 sudo docker load -i ${HOME}/stock-data-analyze-db.tar
