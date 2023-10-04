8e325c965e69e3e42f1dcc5eb34853164bf05cef
dhk <ehdgns322@gmail.com>
2023-10-04 20:14:42 +0900

    opendart api test

--- test/test.py
+++ test/test.py
@@ -1,12 +1,17 @@
 from pykrx.website import krx
 from pykrx import stock
 import datetime
+from backend.stocksimul.custom.opendartreader.dart import OpenDartReader
 
 
-def get_market_ohlcv_by_date(fromdate, todate, ticker):
+def get_market_ohlcv_by_date(fromdate, todate, ticker, adjusted):
     price_info_df = stock.get_market_ohlcv_by_date(fromdate=fromdate, todate=todate,
-                                                   ticker=ticker)
+                                                   ticker=ticker, adjusted=adjusted)
     return price_info_df
+
+
+def get_market_cap_by_date(fromdate, todate, ticker):
+    print(stock.get_market_cap_by_date(fromdate, todate, ticker));
 
 
 def test_datetime():
@@ -24,14 +29,38 @@ def test_dict():
         print(dictionary['005930'])
 
 
+def dart_report():
+    dart = OpenDartReader('69500f38022cc6f7d33956e4690d43499fa10423')
+    report = dart.report('005930', '배당', 2023, reprt_code='11013')
+
+    print(report)
+
+
+def dart_finstate():
+    dart = OpenDartReader('69500f38022cc6f7d33956e4690d43499fa10423')
+    finstate = dart.finstate('삼성전자', 2023, reprt_code='11013')
+    print(finstate)
+
+
+def dart_finstate_all():
+    dart = OpenDartReader('69500f38022cc6f7d33956e4690d43499fa10423')
+    # finstate = dart.finstate_all('삼성전자', 2023, reprt_code='11013')
+    finstate = dart.finstate_all('삼성전자', 2022, reprt_code='11014')
+    print(finstate)
+
+
 if __name__ == "__main__":
     # df = get_market_ohlcv_by_date("20010101", "20190820", "005930")
     # df = get_market_ohlcv_by_date("20191220", "20200227", "000020")
     # df = get_market_ohlcv_by_date("19991220", "20191231", "008480")
     # df = get_market_ohlcv_by_date("20230701", "20230702", "005930")
     # print(len(df))
-    # # df = get_market_ohlcv_by_date("20211221", "20211222", "005930")
-    # # df = get_market_ohlcv_by_date("20200121", "20200222", "005930")
+    # df = get_market_ohlcv_by_date("20211221", "20211222", "005930")
+    # df = get_market_ohlcv_by_date("19970101", "20230923", "005930", True)
     # print(df)
 
-    test_dict()
+    get_market_cap_by_date('20221229','20221229','005930')
+    # get_market_cap_by_date('20140101','20170101','018260')
+    # dart_report()
+    # dart_finstate()
+    # dart_finstate_all()
