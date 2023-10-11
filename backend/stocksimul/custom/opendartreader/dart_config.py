716afd48ba516b66713b1d042f465a3599ef7470
ehdgns565 <ehdgns322@gmail.com>
2023-10-11 23:34:49 +0900

    재무정보 데이터 batch 생성 -3 (같은 정보에 대해 account_id의 코드가 여러개인 경우가 있어 여러개를 고려하도록 수정)

--- backend/stocksimul/custom/opendartreader/dart_config.py
+++ backend/stocksimul/custom/opendartreader/dart_config.py
@@ -11,64 +11,145 @@ class DartConfig:
     table_type_cis = 'CIS'  # 포괄손익계산서
     table_type_cf = 'CF'  # 현금흐름표
 
-    assets_id = 'ifrs-full_CurrentAssets'  # 자산총계
-    liabilities_id = 'ifrs-full_Liabilities'  # 부채총계
-    equity_id = 'ifrs-full_Equity'  # 자본총계
-    revenue_id = 'ifrs-full_Revenue'  # 수익(매출액)
-    operating_income_loss_id = 'dart_OperatingIncomeLoss'  # 영업이익
-    profit_loss_id = 'ifrs-full_ProfitLoss'  # 당기순이익
-    profit_loss_control_id = 'ifrs-full_ProfitLossAttributableToOwnersOfParent'  # 당기순이익(지배)
-    profit_loss_non_control_id = 'ifrs-full_ProfitLossAttributableToNoncontrollingInterests'  # 당기순이익(비지배)
-    profit_loss_before_tax_id = 'ifrs-full_ProfitLossBeforeTax'  # 세전계속사업이익
-    eps_id = 'ifrs-full_BasicEarningsLossPerShare'  # 주당순이익
-    investing_cash_flow_id = 'ifrs-full_CashFlowsFromUsedInInvestingActivities'  # 투자활동현금흐름
-    operating_cash_flow_id = 'ifrs-full_CashFlowsFromUsedInOperatingActivities'  # 영업활동현금흐름
-    financing_cash_flow_id = 'ifrs-full_CashFlowsFromUsedInFinancingActivities'  # 재무활동현금흐름
+    assets_id_list = ['ifrs-full_CurrentAssets', 'ifrs_Assets']  # 자산총계
+    liabilities_id_list = ['ifrs-full_Liabilities', 'ifrs_Liabilities']  # 부채총계
+    equity_id_list = ['ifrs-full_Equity', 'ifrs_Equity']  # 자본총계
+    revenue_id_list = ['ifrs-full_Revenue', 'ifrs_Revenue']  # 수익(매출액)
+    operating_income_loss_id_list = ['dart_OperatingIncomeLoss']  # 영업이익
+    profit_loss_id_list = ['ifrs-full_ProfitLoss', 'ifrs_ProfitLoss']  # 당기순이익
+    profit_loss_control_id_list = ['ifrs-full_ProfitLossAttributableToOwnersOfParent',
+                                   'ifrs_ProfitLossAttributableToOwnersOfParent']  # 당기순이익(지배)
+    profit_loss_non_control_id_list = ['ifrs-full_ProfitLossAttributableToNoncontrollingInterests',
+                                       'ifrs_ProfitLossAttributableToNoncontrollingInterests']  # 당기순이익(비지배)
+    profit_loss_before_tax_id_list = ['ifrs-full_ProfitLossBeforeTax', 'ifrs_ProfitLossBeforeTax']  # 세전계속사업이익
+    eps_id_list = ['ifrs-full_BasicEarningsLossPerShare', 'ifrs_BasicEarningsLossPerShare']  # 주당순이익
+    investing_cash_flow_id_list = ['ifrs-full_CashFlowsFromUsedInInvestingActivities',
+                                   'ifrs_CashFlowsFromUsedInInvestingActivities']  # 투자활동현금흐름
+    operating_cash_flow_id_list = ['ifrs-full_CashFlowsFromUsedInOperatingActivities',
+                                   'ifrs_CashFlowsFromUsedInOperatingActivities']  # 영업활동현금흐름
+    financing_cash_flow_id_list = ['ifrs-full_CashFlowsFromUsedInFinancingActivities',
+                                   'ifrs_CashFlowsFromUsedInFinancingActivities']  # 재무활동현금흐름
 
     def assets_condition(self, df):
-        return (df[self.column_standard_code] == self.assets_id) & (df[self.column_table_type] == self.table_type_bs)
+        assets_id_condition = None
+        for i, item in enumerate(self.assets_id_list):
+            if assets_id_condition is not None:
+                assets_id_condition = assets_id_condition | (df[self.column_standard_code] == item)
+            else:
+                assets_id_condition = df[self.column_standard_code] == item
+        return assets_id_condition & (df[self.column_table_type] == self.table_type_bs)
 
     def liabilities_condition(self, df):
-        return (df[self.column_standard_code] == self.liabilities_id) & (
-                df[self.column_table_type] == self.table_type_bs)
+        liabilities_id_condition = None
+        for i, item in enumerate(self.liabilities_id_list):
+            if liabilities_id_condition is not None:
+                liabilities_id_condition = liabilities_id_condition | (df[self.column_standard_code] == item)
+            else:
+                liabilities_id_condition = df[self.column_standard_code] == item
+        return liabilities_id_condition & (df[self.column_table_type] == self.table_type_bs)
 
     def equity_condition(self, df):
-        return (df[self.column_standard_code] == self.equity_id) & (df[self.column_table_type] == self.table_type_bs)
+        equity_id_condition = None
+        for i, item in enumerate(self.equity_id_list):
+            if equity_id_condition is not None:
+                equity_id_condition = equity_id_condition | (df[self.column_standard_code] == item)
+            else:
+                equity_id_condition = df[self.column_standard_code] == item
+        return equity_id_condition & (df[self.column_table_type] == self.table_type_bs)
 
     def revenue_condition(self, df):
-        return (df[self.column_standard_code] == self.revenue_id) & (df[self.column_table_type] == self.table_type_is)
+        revenue_id_condition = None
+        for i, item in enumerate(self.revenue_id_list):
+            if revenue_id_condition is not None:
+                revenue_id_condition = revenue_id_condition | (df[self.column_standard_code] == item)
+            else:
+                revenue_id_condition = df[self.column_standard_code] == item
+        return revenue_id_condition & (df[self.column_table_type] == self.table_type_is)
 
     def operating_income_loss_condition(self, df):
-        return (df[self.column_standard_code] == self.operating_income_loss_id) & (
-                df[self.column_table_type] == self.table_type_is)
+        operating_income_loss_id_condition = None
+        for i, item in enumerate(self.operating_income_loss_id_list):
+            if operating_income_loss_id_condition is not None:
+                operating_income_loss_id_condition = operating_income_loss_id_condition | (
+                        df[self.column_standard_code] == item)
+            else:
+                operating_income_loss_id_condition = df[self.column_standard_code] == item
+        return operating_income_loss_id_condition & (df[self.column_table_type] == self.table_type_is)
 
     def profit_loss_condition(self, df):
-        return (df[self.column_standard_code] == self.profit_loss_id) & (
-                df[self.column_table_type] == self.table_type_is)
+        profit_loss_id_condition = None
+        for i, item in enumerate(self.profit_loss_id_list):
+            if profit_loss_id_condition is not None:
+                profit_loss_id_condition = profit_loss_id_condition | (df[self.column_standard_code] == item)
+            else:
+                profit_loss_id_condition = df[self.column_standard_code] == item
+        return profit_loss_id_condition & (df[self.column_table_type] == self.table_type_is)
 
     def profit_loss_before_tax_condition(self, df):
-        return (df[self.column_standard_code] == self.profit_loss_before_tax_id) & (
-                df[self.column_table_type] == self.table_type_is)
+        profit_loss_before_tax_id_condition = None
+        for i, item in enumerate(self.profit_loss_before_tax_id_list):
+            if profit_loss_before_tax_id_condition is not None:
+                profit_loss_before_tax_id_condition = profit_loss_before_tax_id_condition | (
+                        df[self.column_standard_code] == item)
+            else:
+                profit_loss_before_tax_id_condition = df[self.column_standard_code] == item
+        return profit_loss_before_tax_id_condition & (df[self.column_table_type] == self.table_type_is)
 
     def profit_loss_control_condition(self, df):
-        return (df[self.column_standard_code] == self.profit_loss_control_id) & (
-                    df[self.column_table_type] == self.table_type_is)
+        profit_loss_control_id_condition = None
+        for i, item in enumerate(self.profit_loss_control_id_list):
+            if profit_loss_control_id_condition is not None:
+                profit_loss_control_id_condition = profit_loss_control_id_condition | (
+                        df[self.column_standard_code] == item)
+            else:
+                profit_loss_control_id_condition = df[self.column_standard_code] == item
+        return profit_loss_control_id_condition & (df[self.column_table_type] == self.table_type_is)
 
     def profit_loss_non_control_condition(self, df):
-        return (df[self.column_standard_code] == self.profit_loss_non_control_id) & (
-                    df[self.column_table_type] == self.table_type_is)
+        profit_loss_non_control_id_condition = None
+        for i, item in enumerate(self.profit_loss_non_control_id_list):
+            if profit_loss_non_control_id_condition is not None:
+                profit_loss_non_control_id_condition = profit_loss_non_control_id_condition | (
+                        df[self.column_standard_code] == item)
+            else:
+                profit_loss_non_control_id_condition = df[self.column_standard_code] == item
+        return profit_loss_non_control_id_condition & (df[self.column_table_type] == self.table_type_is)
 
     def eps_condition(self, df):
-        return (df[self.column_standard_code] == self.eps_id) & (df[self.column_table_type] == self.table_type_is)
+        eps_id_condition = None
+        for i, item in enumerate(self.eps_id_list):
+            if eps_id_condition is not None:
+                eps_id_condition = eps_id_condition | (df[self.column_standard_code] == item)
+            else:
+                eps_id_condition = df[self.column_standard_code] == item
+        return eps_id_condition & (df[self.column_table_type] == self.table_type_is)
 
     def investing_cash_flow_condition(self, df):
-        return (df[self.column_standard_code] == self.investing_cash_flow_id) & (
-                    df[self.column_table_type] == self.table_type_cf)
+        investing_cash_flow_id_condition = None
+        for i, item in enumerate(self.investing_cash_flow_id_list):
+            if investing_cash_flow_id_condition is not None:
+                investing_cash_flow_id_condition = investing_cash_flow_id_condition | (
+                            df[self.column_standard_code] == item)
+            else:
+                investing_cash_flow_id_condition = df[self.column_standard_code] == item
+        return investing_cash_flow_id_condition & (df[self.column_table_type] == self.table_type_cf)
 
     def operating_cash_flow_condition(self, df):
-        return (df[self.column_standard_code] == self.operating_cash_flow_id) & (
-                df[self.column_table_type] == self.table_type_cf)
+        operating_cash_flow_id_condition = None
+        for i, item in enumerate(self.operating_cash_flow_id_list):
+            if operating_cash_flow_id_condition is not None:
+                operating_cash_flow_id_condition = operating_cash_flow_id_condition | (
+                            df[self.column_standard_code] == item)
+            else:
+                operating_cash_flow_id_condition = df[self.column_standard_code] == item
+        return operating_cash_flow_id_condition & (df[self.column_table_type] == self.table_type_cf)
 
     def financing_cash_flow_condition(self, df):
-        return (df[self.column_standard_code] == self.financing_cash_flow_id) & (
-                df[self.column_table_type] == self.table_type_cf)
+        financing_cash_flow_id_condition = None
+        for i, item in enumerate(self.financing_cash_flow_id_list):
+            if financing_cash_flow_id_condition is not None:
+                financing_cash_flow_id_condition = financing_cash_flow_id_condition | (
+                            df[self.column_standard_code] == item)
+            else:
+                financing_cash_flow_id_condition = df[self.column_standard_code] == item
+        return financing_cash_flow_id_condition & (df[self.column_table_type] == self.table_type_cf)
--- test/test.py
+++ test/test.py
@@ -2,6 +2,8 @@ from pykrx.website import krx
 from pykrx import stock
 import datetime
 from backend.stocksimul.custom.opendartreader.dart import OpenDartReader
+from backend.stocksimul.custom.opendartreader.dart_manager import DartManager
+from backend.stocksimul.custom.opendartreader.dart_config import DartConfig
 
 
 def get_market_ohlcv_by_date(fromdate, todate, ticker, adjusted):
@@ -43,10 +45,49 @@ def dart_finstate():
 
 
 def dart_finstate_all():
-    dart = OpenDartReader('69500f38022cc6f7d33956e4690d43499fa10423')
-    # finstate = dart.finstate_all('삼성전자', 2023, reprt_code='11013')
-    finstate = dart.finstate_all('삼성전자', 2022, reprt_code='11014')
+    finstate = DartManager.instance().get_dart().finstate_all('삼성전자', 2015, reprt_code='11011')
     print(finstate)
+    if finstate is not None:
+        assets = int(finstate.loc[DartConfig().assets_condition(finstate)].iloc[0][DartConfig().column_amount])
+        liabilities = int(
+            finstate.loc[DartConfig().liabilities_condition(finstate)].iloc[0][DartConfig().column_amount])
+        equity = int(finstate.loc[DartConfig().equity_condition(finstate)].iloc[0][DartConfig().column_amount])
+        revenue = int(finstate.loc[DartConfig().revenue_condition(finstate)].iloc[0][DartConfig().column_amount])
+        profit_loss = int(
+            finstate.loc[DartConfig().profit_loss_condition(finstate)].iloc[0][DartConfig().column_amount])
+        operating_income_loss = int(finstate.loc[DartConfig().operating_income_loss_condition(finstate)].iloc[0][
+                                        DartConfig().column_amount])
+        profit_loss_control = int(finstate.loc[DartConfig().profit_loss_control_condition(finstate)].iloc[0][
+                                      DartConfig().column_amount])
+        profit_loss_non_control = int(finstate.loc[DartConfig().profit_loss_non_control_condition(finstate)].iloc[0][
+                                          DartConfig().column_amount])
+        profit_loss_before_tax = int(finstate.loc[DartConfig().profit_loss_before_tax_condition(finstate)].iloc[0][
+                                         DartConfig().column_amount])
+        eps = int(finstate.loc[DartConfig().eps_condition(finstate)].iloc[0][DartConfig().column_amount])
+        investing_cash_flow = int(finstate.loc[DartConfig().investing_cash_flow_condition(finstate)].iloc[0][
+                                      DartConfig().column_amount])
+        operating_cash_flow = int(finstate.loc[DartConfig().operating_cash_flow_condition(finstate)].iloc[0][
+                                      DartConfig().column_amount])
+        financing_cash_flow = int(finstate.loc[DartConfig().financing_cash_flow_condition(finstate)].iloc[0][
+                                      DartConfig().column_amount])
+
+        print(
+            'assets={}\n'
+            'liabilities={}\n'
+            'equity={}\n'
+            'revenue={}\n'
+            'profit_loss={}\n'
+            'operating_income_loss={}\n'
+            'profit_loss_control={}\n'
+            'profit_loss_non_control={}\n'
+            'profit_loss_before_tax={}\n'
+            'eps={}\n'
+            'investing_cash_flow={}\n'
+            'operating_cash_flow={}\n'
+            'financing_cash_flow={}\n'.format(assets, liabilities, equity, revenue, profit_loss, operating_income_loss,
+                                              profit_loss_control, profit_loss_non_control, profit_loss_before_tax, eps,
+                                              investing_cash_flow, operating_cash_flow, financing_cash_flow
+                                              ))
 
 
 if __name__ == "__main__":
@@ -59,8 +100,8 @@ if __name__ == "__main__":
     # df = get_market_ohlcv_by_date("19970101", "20230923", "005930", True)
     # print(df)
 
-    get_market_cap_by_date('20221229','20221229','005930')
+    # get_market_cap_by_date('20221229','20221229','005930')
     # get_market_cap_by_date('20140101','20170101','018260')
     # dart_report()
     # dart_finstate()
-    # dart_finstate_all()
+    dart_finstate_all()
